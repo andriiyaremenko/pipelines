228fbdf790db1a26df94112d2a96769347931f57
Andrii Yaremenko <yarrem@gmail.com>
2022-03-22 17:56:15 +0200

    shorter type names (T.Name() instead of T.String())

--- internal/helpers.go
+++ internal/helpers.go
@@ -9,10 +9,10 @@ func ZeroValue[T any]() T {
 
 func TypeName[T any]() string {
 	t := reflect.TypeOf((*T)(nil))
-	return t.Elem().String()
+	return t.Elem().Name()
 }
 
 func InstanceTypeName(instance any) string {
 	t := reflect.TypeOf(instance)
-	return t.String()
+	return t.Name()
 }
